f6ab912b518f6c32594bc2c89242cf02b87caeb9
Aryan0Jain <aryanajjain@gmail.com>
2023-12-16 16:16:04 +0530

    Final Commit

--- index.js
+++ index.js
@@ -5,26 +5,27 @@ const historyTab = document.querySelector("#history");
 const historyContainer = document.querySelector(".history-container");
 const searchTab = document.querySelector("#search");
 const searchContainer = document.querySelector(".search-container");
+const confirmBox = document.querySelector(".confirm");
 const URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
 if (!localStorage.getItem("searches"))
 	localStorage.setItem("searches", JSON.stringify([]));
 
 searchInput.addEventListener("input", () => {
-	result.innerHTML = "Waiting...";
+	if (searchInput.value == "")
+		result.innerHTML =
+			"Type a word and press enter or click on search button.";
+	else result.innerHTML = "Waiting...";
 });
 function searchWord() {
 	let word = searchInput.value;
-	result.innerHTML = "Finding...";
+	result.innerHTML = "";
 	if (word == "") {
-		// result.style.display = "block"
 		result.innerHTML = "Please Enter A Word!";
-		result.classList.add("error");
-		result.classList.remove("correct");
 		return;
 	}
-	result.classList.add("correct");
-	result.classList.remove("error");
+	result.id = "loader";
+	word = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
 	searchInput.value = "";
 	searchAndDisplay(word);
 }
@@ -40,14 +41,14 @@ async function searchAndDisplay(word) {
 		const example = res.meanings[0].definitions[0].example ?? "";
 		const audio = res.phonetics.filter((cur) => cur.audio)?.[0]?.audio;
 		const wordObj = { word, meaning, audio, example };
+		result.id = "";
 		result.style.display = "block";
 		addToStorage(wordObj);
 		displayWord(wordObj);
 	} catch (e) {
 		console.log(e);
-		result.innerHTML = `No such word found: ${word}`;
-		result.classList.add("error");
-		result.classList.remove("correct");
+		result.id = "";
+		result.innerHTML = `No such word found: <span id="not-a-word">${word}</span>`;
 	}
 }
 
@@ -59,10 +60,10 @@ function addToStorage(obj) {
 	localStorage.setItem("searches", JSON.stringify(words));
 }
 function displayWord({ word, meaning, audio, example }) {
-	let audioHtml = `Listen: 
-        <button class="play" onClick="handleAudioclick('${audio}')">
+	let audioHtml = `
+        <button onClick="handleAudioclick('${audio}')">
             <audio id="play-btn" src="${audio}"></audio>
-            <i class="fa-solid fa-play"></i>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zM188.3 147.1c7.6-4.2 16.8-4.1 24.3 .5l144 88c7.1 4.4 11.5 12.1 11.5 20.5s-4.4 16.1-11.5 20.5l-144 88c-7.4 4.5-16.7 4.7-24.3 .5s-12.3-12.2-12.3-20.9V168c0-8.7 4.7-16.7 12.3-20.9z"/></svg>
         </button>
     `;
 	let exampleHtml = `<p class="example"><span>Example: </span>${example}</p>`;
@@ -90,41 +91,64 @@ historyTab.addEventListener("click", () => {
 	historyContainer.style.display = "block";
 	displayHistory();
 });
-searchTab.addEventListener("click", () => {
+searchTab.addEventListener("click", displaySearchPage);
+function displaySearchPage() {
 	searchTab.style.display = "none";
 	historyTab.style.display = "block";
 	searchContainer.style.display = "block";
 	result.style.display = "flex";
 	historyContainer.style.display = "none";
-	// result.innerHTML = "";
-});
+	result.innerHTML = "Type a word and press enter or click on search button.";
+}
 
 function displayHistory() {
 	const words = JSON.parse(localStorage.getItem("searches"));
 	let html = "";
-	words.forEach(({ word, meaning }, index) => {
-		html += `
-            <div class="word">
-                <p class="title">${word}</p>
-				<button class="delete" id=${index}>
-					<i class="fa-regular fa-trash-can"></i>
-				</button>
-                <p class="meaning">
-                    ${meaning}
-                </p>
-            </div>
-        `;
-	});
+	if (words.length == 0) {
+		html = `<div class="empty-history">All you searched words will be availabe here.</div>`;
+	} else {
+		words.forEach(({ word, meaning }, index) => {
+			html += `
+			<div class="word">
+			<div class="wrap">
+			<p class="title" onclick="handleWordCardClick('${word}')">${word}</p>
+			<button class="delete" onclick="deleteCard('${index}','${word}')" id=${index}>
+			<i class="fa-regular fa-trash-can"></i>
+			</button>
+			</div>
+			<p class="meaning">
+			${meaning}
+			</p>
+				</div>
+				`;
+		});
+	}
 	historyContainer.innerHTML = `<h3>Your History:</h3>
-    <div class="words">${html}</div>`;
-	[...document.querySelectorAll(".delete")].forEach((item) => {
-		item.addEventListener("click", () => {
-			let i = item.id;
-			item.parentElement.remove();
-			let words = JSON.parse(localStorage.getItem("searches"));
-			words.splice(i, 1);
+		<div class="words">${html}</div>`;
+}
+// function deleteCard(index, word) {
+// 	confirmUser(word, index);
+// }
+function handleWordCardClick(word) {
+	displaySearchPage();
+	searchInput.value = word;
+	searchWord();
+}
+function deleteCard(index, word) {
+	const spanInsideConfirm = document.querySelector(".confirm span");
+	spanInsideConfirm.innerHTML = word;
+	confirmBox.style.display = "flex";
+	const confirmButtons = document.querySelector(".confirm .buttons");
+	confirmButtons.addEventListener("click", (e) => {
+		if (e.target.innerHTML == "Yes") {
+			confirmBox.style.display = "none";
+			const words = JSON.parse(localStorage.getItem("searches"));
+			words.splice(index, 1);
 			localStorage.setItem("searches", JSON.stringify(words));
 			displayHistory();
-		});
+		} else if (e.target.innerHTML == "No") {
+			shouldDelete = false;
+			confirmBox.style.display = "none";
+		}
 	});
 }
